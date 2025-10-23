0ab6728749c66cad6912fdd73aa761215d59d3b0
justinchin <justinchin.professionalcareer@gmail.com>
2025-10-23 18:25:56 -0400

    experimenting on sg

--- terraform/security.tf
+++ terraform/security.tf
@@ -10,6 +10,20 @@ resource "aws_security_group" "eks_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
