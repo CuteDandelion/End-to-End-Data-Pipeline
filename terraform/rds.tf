3526dce114713d36bd819670095bbe241c4dd052
justinchin <justinchin.professionalcareer@gmail.com>
2025-10-23 09:09:24 -0400

    introduce new RDS security group

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -7,6 +7,28 @@ resource "aws_db_subnet_group" "rds_subnets" {
   }
 }
 
+resource "aws_security_group" "rds_db_sg" {
+  name   = "rds-db-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description      = "Allow DB access from EKS nodes"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    security_groups  = [aws_security_group.eks_cluster_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 resource "aws_db_instance" "rds" {
   identifier        = "${var.project_name}-rds"
   engine            = "postgres"
@@ -16,6 +38,6 @@ resource "aws_db_instance" "rds" {
   password          = var.db_password
   skip_final_snapshot = true
   publicly_accessible = false
-  vpc_security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_db_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnets.name
 }
